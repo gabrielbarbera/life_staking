d985c0ad27b2b0c35a7bbee4f87ab8314a1ecfc0
Gabriel Barbera <gabrielbarbera@gmail.com>
2023-09-19 18:42:25 +0200

    Staking and Bridge Final

--- js/scripts.js
+++ js/scripts.js
@@ -76,20 +76,32 @@ $(document).ready(function() {
     });
 
 
-    // new DataTable('#life_staking_table', {
-    //     responsive: {
-    //         details: {
-    //             display: DataTable.Responsive.display.modal({
-    //                 header: function (row) {
-    //                     var data = row.data();
-    //                     return 'Details for ' + data[0] + ' ' + data[1];
-    //                 }
-    //             }),
-    //             renderer: DataTable.Responsive.renderer.tableAll({
-    //                 tableClass: 'table'
-    //             })
+    // $(function () {
+
+    //     $(".progress").each(function () {
+
+    //         var value = $(this).attr('data-value');
+    //         var left = $(this).find('.progress-left .progress-bar');
+    //         var right = $(this).find('.progress-right .progress-bar');
+
+    //         if (value > 0) {
+    //             if (value <= 50) {
+    //                 right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+    //             } else {
+    //                 right.css('transform', 'rotate(180deg)')
+    //                 left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+    //             }
     //         }
+
+    //     })
+
+    //     function percentageToDegrees(percentage) {
+
+    //         return percentage / 100 * 360
+
     //     }
+
     // });
+
 
 });
